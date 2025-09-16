78961affebc25e461d2e497196c3691c8524a283
shafay gilani <shafaygilani4@gmail.com>
2025-09-16 06:14:26 +0000

    check whole website and fix the issue i it

--- src/app/(admin)/admin/notifications/page.tsx
+++ src/app/(admin)/admin/notifications/page.tsx
@@ -195,7 +195,7 @@ export default function AdminNotificationsPage() {
                 <Button size="sm" className="h-7 gap-1" onClick={handleAddNew}>
                   <PlusCircle className="h-3.5 w-3.5" />
                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Add Item
+                    Add Notification
                   </span>
                 </Button>
               </DialogTrigger>
